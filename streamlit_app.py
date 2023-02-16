bb92229ff4af29ba4732b5ff8adca3317b1fdf37
Aman Dayal <118193512+Aman-Dayal@users.noreply.github.com>
2023-02-16 13:28:41 +0530

    Update streamlit_app.py
    try except block added with nested if -else

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,30 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 #functionality to choose fruits for users and get details about fruit from fruity vice api
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice:
+        streamlit.error('Please select a fruit to get information.')
+   else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      #normalize json response 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      #output response to a dataframe
+      streamlit.dataframe(fruityvice_normalized)
+      
+except URLError as e:
+streamlit.error()
 
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
-#normalize json response 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output response to a dataframe
-streamlit.dataframe(fruityvice_normalized)
+
+
+
+
+
 
 streamlit.stop()
 
@@ -58,5 +70,5 @@ add_my_fruit = streamlit.text_input("What fruit would you like to choose?")
 streamlit.write("Thanks for adding",add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
-
+streamlit.write('The user entered ', fruit_choice)
 
